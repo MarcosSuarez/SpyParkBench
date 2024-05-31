3367e9830cfae87dbf44a573c0943fdb3446d9d5
Marcos Suárez <marcos.sua.aya@gmail.com>
2024-05-31 11:30:36 +0200

    Initial Commit

---
+++ SpyParkBench/SpyParkBenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyParkBenchApp.swift
+//  SpyParkBench
+//
+//  Created by Marcos Suarez Ayala on 31/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SpyParkBenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpyParkBenchUITests/SpyParkBenchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpyParkBenchUITestsLaunchTests.swift
+//  SpyParkBenchUITests
+//
+//  Created by Marcos Suarez Ayala on 31/5/24.
+//
+
+import XCTest
+
+final class SpyParkBenchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
